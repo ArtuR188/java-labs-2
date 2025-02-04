c34bc6262568a5730f99b0dc13cde9c01a9a7f57
ArtuR188 <144206907+ArtuR188@users.noreply.github.com>
2025-02-04 11:19:01 +0200

    Add files via upload

--- Main.java
+++ Main.java
@@ -1,7 +1,6 @@
 public class Main {
     public static void main(String[] args) {
-        int a = 17, b = 5;
-        System.out.println("fraction: " + (a / b));
-        System.out.println("remainder: " + (a % b));
+        double num = 5.7;
+        System.out.println("rounded: " + Math.round(num));
     }
 }
